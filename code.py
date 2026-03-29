69d0ba9d806532107a0c1f5d0df327302ae68756
Ayan Nalawade <ayannalawade@gmail.com>
2026-03-29 19:20:04 -0400

    Sun Mar 29 19:20:04 EDT 2026

--- code.py
+++ code.py
@@ -1,7 +1,14 @@
 from tkinter import *
+import time
+import math 
 
-s = Canvas(Tk(), width=800, height=600, background="white")
+r = Tk()
+r.title("Mario Yeehaw")
+
+s = Canvas(r, width=800, height=600, bg="#5C94FC")
 s.pack()
+
+
 
 
 s.mainloop()
