20df22c0c91fd5ea3061a492e7556cac645aeb7b
Ayan <ayannalawade@gmail.com>
2026-04-27 17:08:02 -0400

    Mon Apr 27 17:08:02 EDT 2026

--- game.py
+++ game.py
@@ -33,14 +33,22 @@ f.pack(fill="both", expand=True) # Only after the user picks we want to create t
 class Gameboard:
     def __init__(self):
         self.gamestate = []
-        self.colors = ["b", "g", "o", "p", "r", "g"]
+        self.colors = ["blue", "green", "red", "purple", "orange"]
     
     def draw_instructions(self):
         f.create_rectangle(20, 20, WIDTH-350, HEIGHT-20, fill="#3B3737")
-        f.create_text(130, 40, text="Click on the colored peg \nTo place into the row", fill="white")
+        f.create_text(135, 50, text="Click on the colored peg \nto place into the row", fill="white")
+    
+    def on_ball_click(self, event):
+        item = f.find_withtag("current") # Current just finds the tag name for whatever element the cursor was on DURING the click.
+
+        if item: # If it doesn't exist, maybe the user used a machine to move the cusor faster than the command ran, then we want to do nothing 
+            tag = f.gettags(item[0])
+            print(tag)
+            print(f"I clicked {tag[1]} ball!!!")
     
     def draw_balls(self):
-        start_x = 35 # x coordinate
+        start_x = 50 # x coordinate
         y = 100 # y coordinate
         dimeter = 25 # Diameter of the ball
         spce = 35 # Space between each balls
@@ -49,14 +57,16 @@ class Gameboard:
             x = start_x + (i*spce)
             f.create_oval(x, y, x+dimeter, y+dimeter,
                           fill=self.colors[i],
-                          outlint="#4a4a4a",
+                          outline="#4a4a4a",
                           width=2,
                           tags=("ball",self.colors[i])) # Assign tag ball and the color. Ball isn't required here since its a easy animation but added it as practice
-        
+        f.tag_bind("ball", "<Button-1>", self.on_ball_click)
+
 
 
 
 game = Gameboard()
 game.draw_instructions()
+game.draw_balls()
 
 f.mainloop()
