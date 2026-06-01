ccb2aa9bc4e94e9bca7156b1f74e203b902532b5
Ayan <ayannalawade@gmail.com>
2026-06-01 15:53:11 -0400

    Mon Jun  1 15:53:11 EDT 2026: Adds progression

--- main.py
+++ main.py
@@ -27,14 +27,37 @@ class Board:
     def __init__(self):
         self.sqh = HEIGHT//8 # Get the square height required for each square of the board
         self.sqw = WIDTH//8 # Get the square width required for each square of the board
+    
+
+    def __progression(self, character) -> str:
+        one, two = list(character)
+        one = ord(one) # Convert to ASCII
+        if one == 72: # If H
+            if two == 8 or f"A{int(two)+1}" == "A9": # Check if the second value is a 8 AND make sure its not A9. A9 is possible since the +1 for two is done AFTER the check
+                return "None"
+            
+            return f"A{int(two)+1}"
+        return f"{chr(one+1)}{two}"
+        
+    def progression_test(self):
+        start = "A1"
+        for i in range(64):
+            x = self.__progression(start)
+            start = x
+            print(x)
 
     def draw_board(self):
         for i in range(0,8): # Each row
             for x in range(0,8): # Each coloumn
                 f.create_rectangle(x * self.sqw, i * self.sqh, (x+1) * self.sqw, (i+1) * self.sqh)
+                centrex = x * self.sqw + self.sqw // 2  # center x of square
+                centrey = i * self.sqh + self.sqh // 2  # center y of square
+                # Centre will be used to actually move the pieces
+
+
 
 
 c = Board()
 c.draw_board()
-
+c.progression_test()
 f.mainloop()
